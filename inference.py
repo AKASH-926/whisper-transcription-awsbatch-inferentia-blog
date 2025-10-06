b6a898017fb52550c21a24d3a15b5735514161c8
Akash <akash.arthanur26@gmail.com>
2025-10-06 10:01:04 +0530

    revert again to approximation

--- inference.py
+++ inference.py
@@ -1,26 +1,17 @@
 import os
 import sys
+os.environ['NEURON_RT_NUM_CORES']='1'
 import types
 import torch
-import torchaudio
-import torch.nn.functional as F
 from datasets import load_dataset
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
-from transformers.modeling_outputs import BaseModelOutputWithPastAndCrossAttentions, BaseModelOutput
 import boto3
-import time
-import re
-import torch_neuronx
 
-# -----------------------------
-# Environment & arguments
-# -----------------------------
-os.environ['NEURON_RT_NUM_CORES'] = '1'
 input_bucket_name = sys.argv[1]
 input_file_key = sys.argv[2]
 
 output_bucket_name = os.environ['OUTPUT_BUCKET_NAME']
-output_file_prefix = os.environ['OUTPUT_FILE_PREFIX']
+output_file_prefix =  os.environ['OUTPUT_FILE_PREFIX']
 
 model_artifact_bucket_name = os.environ['MODEL_BUCKET_NAME']
 model_artifact_encoder_key = os.environ['MODEL_ENCODER_S3_KEY']
@@ -29,24 +20,44 @@ model_artifact_proj_key = os.environ['MODEL_PROJ_S3_KEY']
 
 s3_client = boto3.client('s3')
 
-# -----------------------------
-# Model setup
-# -----------------------------
-suffix = "large-v3"
-model_id = f"openai/whisper-{suffix}"
+# please, start by selecting the desired model size
+#suffix="tiny"
+#suffix="small"
+#suffix="medium"
+suffix="large-v3"
+model_id=f"openai/whisper-{suffix}"
+
+# this will load the tokenizer + two copies of the model.
 processor = WhisperProcessor.from_pretrained(model_id)
 model = WhisperForConditionalGeneration.from_pretrained(model_id, torchscript=True)
 
-batch_size = 1
-output_attentions = True
+# batch size refers to the number of files processed in parallel
+# and should not exceed the number of cores on the Neuron device.
+batch_size=1
+
+# output_attentions is required if you want to return word timestamps
+# if you don't need timestamps, just set this to False and get some better latency
+output_attentions=True
+
+# this is the maximum number of tokens the model will be able to decode
+# for the sample #3 we selected above, this is enough. If you're planning to 
+# process larger samples, you need to adjust it accordinly.
 max_dec_len = 448
-dim_enc = model.config.num_mel_bins
-dim_dec = model.config.d_model
+# num_mel_bins,d_model --> these parameters where copied from model.conf (found on HF repo)
+# we need them to correctly generate dummy inputs during compilation
+dim_enc=model.config.num_mel_bins
+dim_dec=model.config.d_model
 print(f'Dim enc: {dim_enc}; Dim dec: {dim_dec}')
 
-# -----------------------------
-# Neuron forward overrides
-# -----------------------------
+
+import types
+import torch.nn.functional as F
+from transformers.modeling_outputs import BaseModelOutputWithPastAndCrossAttentions,BaseModelOutput
+
+# Now we need to simplify both encoding & decoding forward methods to make them 
+# compilable. Please notice that these methods overwrite the original ones, but
+# keeps retro-compatibility. Also, we'll use use a new variable "forward_neuron"
+# to invoke the model on inf2
 def enc_f(self, input_features, attention_mask, **kwargs):
     if hasattr(self, 'forward_neuron'):
         out = self.forward_neuron(input_features, attention_mask)
@@ -55,151 +66,239 @@ def enc_f(self, input_features, attention_mask, **kwargs):
     return BaseModelOutput(**out)
 
 def dec_f(self, input_ids, attention_mask=None, encoder_hidden_states=None, **kwargs):
+    out = None        
+    if not attention_mask is None and encoder_hidden_states is None:
+        # this is a workaround to align the input parameters for NeuronSDK tracer
+        # None values are not allowed during compilation
+        encoder_hidden_states, attention_mask = attention_mask,encoder_hidden_states
     inp = [input_ids, encoder_hidden_states]
+    
+    # pad the input to max_dec_len
     if inp[0].shape[1] > self.max_length:
         raise Exception(f"The decoded sequence is not supported. Max: {self.max_length}")
     pad_size = torch.as_tensor(self.max_length - inp[0].shape[1])
     inp[0] = F.pad(inp[0], (0, pad_size), "constant", processor.tokenizer.pad_token_id)
-
+    
     if hasattr(self, 'forward_neuron'):
         out = self.forward_neuron(*inp)
     else:
-        out = self.forward_(input_ids=inp[0], encoder_hidden_states=inp[1],
-                            return_dict=True, use_cache=False, output_attentions=output_attentions)
-
+        # output_attentions is required if you want timestamps
+        out = self.forward_(input_ids=inp[0], encoder_hidden_states=inp[1], return_dict=True, use_cache=False, output_attentions=output_attentions)
+    # unpad the output
     out['last_hidden_state'] = out['last_hidden_state'][:, :input_ids.shape[1], :]
-    if out.get('attentions') is not None:
-        out['attentions'] = torch.stack([torch.mean(o[:, :, :input_ids.shape[1], :input_ids.shape[1]], axis=2, keepdim=True)
-                                         for o in out['attentions']])
-    if out.get('cross_attentions') is not None:
-        out['cross_attentions'] = torch.stack([torch.mean(o[:, :, :input_ids.shape[1], :], axis=2, keepdim=True)
-                                               for o in out['cross_attentions']])
+    # neuron compiler doesn't like tuples as values of dicts, so we stack them into tensors
+    # also, we need to average axis=2 given we're not using cache (use_cache=False)
+    # that way, to avoid an issue with the pipeline we change the shape from:
+    #  bs,num selected,num_tokens,1500 --> bs,1,num_tokens,1500
+    # I suspect there is a bug in the HF pipeline code that doesn't support use_cache=False for
+    # word timestamps, that's why we need that.
+    if not out.get('attentions') is None:
+        out['attentions'] = torch.stack([torch.mean(o[:, :, :input_ids.shape[1], :input_ids.shape[1]], axis=2, keepdim=True) for o in out['attentions']])
+    if not out.get('cross_attentions') is None:
+        out['cross_attentions'] = torch.stack([torch.mean(o[:, :, :input_ids.shape[1], :], axis=2, keepdim=True) for o in out['cross_attentions']])
     return BaseModelOutputWithPastAndCrossAttentions(**out)
+
+if not hasattr(model.model.encoder, 'forward_'): 
+    model.model.encoder.forward_ = model.model.encoder.forward
+if not hasattr(model.model.decoder, 'forward_'): 
+    model.model.decoder.forward_ = model.model.decoder.forward
+if not hasattr(model.proj_out, 'forward_'):
+    model.proj_out.forward_ = model.proj_out.forward
 
 def proj_out_f(self, inp):
     pad_size = torch.as_tensor(self.max_length - inp.shape[1], device=inp.device)
-    x = F.pad(inp, (0, 0, 0, pad_size), "constant", 0.0)  # pad with 0.0 for features
+    # pad the input to max_dec_len
+    if inp.shape[1] > self.max_length:
+        raise Exception(f"The decoded sequence is not supported. Max: {self.max_length}")
+    x = F.pad(inp, (0,0,0,pad_size), "constant", processor.tokenizer.pad_token_id)
+
     if hasattr(self, 'forward_neuron'):
         out = self.forward_neuron(x)
     else:
         out = self.forward_(x)
-    return out[:, :inp.shape[1], :]
-
-for m in [model.model.encoder, model.model.decoder, model.proj_out]:
-    if not hasattr(m, 'forward_'):
-        m.forward_ = m.forward
+    # unpad the output before returning
+    out = out[:, :inp.shape[1], :]
+    return out
 
 model.model.encoder.forward = types.MethodType(enc_f, model.model.encoder)
 model.model.decoder.forward = types.MethodType(dec_f, model.model.decoder)
 model.proj_out.forward = types.MethodType(proj_out_f, model.proj_out)
+
 model.model.decoder.max_length = max_dec_len
 model.proj_out.max_length = max_dec_len
 
-# -----------------------------
-# Load Neuron model artifacts from S3
-# -----------------------------
-for s3_key, attr in [(model_artifact_encoder_key, 'encoder'),
-                     (model_artifact_decoder_key, 'decoder'),
-                     (model_artifact_proj_key, 'proj_out')]:
-    local_file = s3_key.split('/')[-1]
-    s3_client.download_file(model_artifact_bucket_name, s3_key, local_file)
-    if not os.path.isfile(local_file):
-        raise Exception(f"{attr} model artifact not found.")
-    setattr(model.model if attr != 'proj_out' else model, f"{attr}.forward_neuron",
-            torch.jit.load(local_file, map_location=torch.device("cpu")))
+# Trace Encoder
+import os
+import torch
+import torch_neuronx
 
-# -----------------------------
-# Load input audio from S3
-# -----------------------------
-local_audio_file = input_file_key.split("/")[-1]
-s3_client.download_file(input_bucket_name, input_file_key, local_audio_file)
-waveform, sample_rate = torchaudio.load(local_audio_file)
+# download model artifacts from S3
+s3_client.download_file(model_artifact_bucket_name, model_artifact_encoder_key, model_artifact_encoder_key.split("/")[-1])
+model_encoder_filename=model_artifact_encoder_key.split("/")[-1]
+if not os.path.isfile(model_encoder_filename):
+    raise Exception("encoder model artifact not found.")
+else:
+    model.model.encoder.forward_neuron = torch.jit.load(model_encoder_filename)
+    
+# Trace Decoder
+import torch
+import torch_neuronx
 
-# Mono & 16kHz
+s3_client.download_file(model_artifact_bucket_name, model_artifact_decoder_key, model_artifact_decoder_key.split("/")[-1])
+model_decoder_filename=model_artifact_decoder_key.split("/")[-1]
+if not os.path.isfile(model_decoder_filename):
+    raise Exception("decoder model artifact not found.")
+else:
+    model.model.decoder.forward_neuron = torch.jit.load(model_decoder_filename)
+    
+# Trace Projection Output
+import torch
+import torch_neuronx
+
+s3_client.download_file(model_artifact_bucket_name, model_artifact_proj_key, model_artifact_proj_key.split("/")[-1])
+model_proj_filename=model_artifact_proj_key.split("/")[-1]
+if not os.path.isfile(model_proj_filename):
+    raise Exception("projection model artifact not found.")
+else:
+    model.proj_out.forward_neuron = torch.jit.load(model_proj_filename)
+
+# Inference
+import torchaudio
+
+# copy from s3
+s3_client.download_file(input_bucket_name, input_file_key, input_file_key.split("/")[-1])
+audio_path = input_file_key.split("/")[-1]
+
+# Load the audio file
+waveform, sample_rate = torchaudio.load(audio_path)
+
+# Ensure the audio is in the correct format (mono, 16kHz)
 if waveform.shape[0] > 1:
     waveform = torch.mean(waveform, dim=0, keepdim=True)
 if sample_rate != 16000:
     waveform = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=16000)(waveform)
 
 # -----------------------------
-# Silence/Music detection
+# Chunking with overlap
 # -----------------------------
-def detect_silence(waveform, sample_rate, frame_size=1024, hop_size=512, threshold=0.01):
-    waveform = waveform.mean(dim=0)
-    num_frames = (waveform.shape[0] - frame_size) // hop_size + 1
-    segments, current_start, is_speech = [], 0, False
-
-    for i in range(num_frames):
-        start = i * hop_size
-        frame = waveform[start:start + frame_size]
-        energy = torch.sqrt(torch.mean(frame ** 2))
-        speech_frame = float(energy) > threshold
-
-        if speech_frame != is_speech:
-            if i > 0:
-                segments.append({
-                    "start": current_start / sample_rate,
-                    "end": start / sample_rate,
-                    "speech": is_speech
-                })
-            current_start = start
-            is_speech = speech_frame
-
-    segments.append({
-        "start": current_start / sample_rate,
-        "end": waveform.shape[0] / sample_rate,
-        "speech": is_speech
-    })
-    return segments
-
-segments = detect_silence(waveform, 16000)
+chunk_size = 30 * 16000        # 30 seconds
+overlap = 5 * 16000            # 5 seconds overlap to reduce missed words
+chunks, start = [], 0
+while start < waveform.shape[1]:
+    end = min(start + chunk_size, waveform.shape[1])
+    chunks.append(waveform[:, max(0, start - overlap):end])
+    start += chunk_size
 
 # -----------------------------
-# Inference & timestamp generation
+# Inference with precise sentence-level timestamps
 # -----------------------------
+import re
+import time
+import torch
+
+t = time.time()
 all_sentences = []
+current_time = 0.0  # Track total elapsed time across chunks
+leftover_text = ""
+leftover_time = 0.0
 
-for seg in segments:
-    seg_wave = waveform[:, int(seg['start'] * 16000):int(seg['end'] * 16000)]
-    duration = seg['end'] - seg['start']
-
-    if not seg['speech']:
-        all_sentences.append({"text": "[Music / Silence]", "start": seg['start'], "end": seg['end']})
-        continue
-
-    inputs = processor(seg_wave.squeeze().numpy(), sampling_rate=16000, return_tensors="pt")
+for chunk in chunks:
+    # Convert and process audio
+    inputs = processor(chunk.squeeze().numpy(), sampling_rate=16000, return_tensors="pt")
     with torch.no_grad():
         predicted_ids = model.generate(inputs.input_features)
     transcription = processor.decode(predicted_ids[0], skip_special_tokens=True).strip()
+
+    print("transcription:", transcription)
+
+    chunk_duration = chunk.shape[1] / 16000  # seconds
+
     if not transcription:
-        all_sentences.append({"text": "[Unintelligible]", "start": seg['start'], "end": seg['end']})
+        # If no speech detected → mark as music/silence
+        all_sentences.append({
+            "text": "[Music / Silence]",
+            "start": current_time,
+            "end": current_time + chunk_duration
+        })
+        current_time += chunk_duration
         continue
 
+    # Merge leftover text from previous chunk (avoid double spacing)
+    if leftover_text:
+        transcription = (leftover_text + " " + transcription).strip()
+        word_start_time = leftover_time
+        leftover_text, leftover_time = "", 0.0
+    else:
+        word_start_time = current_time
+
+    # Sentence-level timestamps with proportional timing
     words = transcription.split()
-    total_tokens = len(words)
-    duration_per_word = duration / max(total_tokens, 1)
-    word_start = seg['start']
+    if not words:
+        current_time += chunk_duration
+        continue
 
-    for word in words:
-        word_end = word_start + duration_per_word
-        all_sentences.append({
-            "text": word,
-            "start": word_start,
-            "end": word_end
-        })
-        word_start = word_end
+    # Adjust time ratio based on full text (including spaces)
+    total_chars = len(transcription)
+    char_time_ratio = chunk_duration / total_chars
+
+    sentence = {"text": "", "start": None, "end": None}
+
+    for i, word in enumerate(words):
+        word_duration = len(word) * char_time_ratio
+        start = word_start_time
+        end = start + word_duration
+
+        if sentence["start"] is None:
+            sentence["start"] = start
+        sentence["text"] += word + " "
+        sentence["end"] = end
+
+        word_start_time = end
+
+        # If word ends with punctuation → finalize sentence
+        if re.search(r'[.?!]$', word):
+            all_sentences.append(sentence)
+            sentence = {"text": "", "start": None, "end": None}
+
+    # If chunk ended mid-sentence → carry over text to next chunk
+    if sentence["text"].strip():
+        leftover_text = sentence["text"].strip()
+        leftover_time = sentence["start"]
+    else:
+        leftover_text = ""
+        leftover_time = 0.0
+
+    current_time += chunk_duration
+
+# Final flush for leftover text
+if leftover_text.strip():
+    all_sentences.append({
+        "text": leftover_text.strip(),
+        "start": leftover_time,
+        "end": current_time
+    })
+
+print(f"Elapsed inference: {time.time()-t}")
+
+# Combine transcriptions with sentence-level timestamps for txt
+full_transcription = "\n".join(
+    [f"[{s['start']:.2f}s - {s['end']:.2f}s] {s['text'].strip()}" for s in all_sentences]
+)
+
+# Save locally
+output_filename = audio_path + '.txt'
+with open(output_filename, 'w') as file:
+    file.write(full_transcription)
+
+# Upload to S3
+s3_client.put_object(
+    Body=full_transcription,
+    Bucket=output_bucket_name,
+    Key=output_file_prefix + output_filename
+)
 
 # -----------------------------
-# Save TXT
-# -----------------------------
-full_transcription = "\n".join([f"[{s['start']:.2f}s - {s['end']:.2f}s] {s['text'].strip()}" for s in all_sentences])
-txt_filename = local_audio_file + ".txt"
-with open(txt_filename, "w") as f:
-    f.write(full_transcription)
-s3_client.put_object(Body=full_transcription, Bucket=output_bucket_name, Key=output_file_prefix + txt_filename)
-
-# -----------------------------
-# Save SRT
+# Function to save SRT
 # -----------------------------
 def seconds_to_srt_time(seconds: float) -> str:
     hours = int(seconds // 3600)
@@ -208,27 +307,48 @@ def seconds_to_srt_time(seconds: float) -> str:
     millis = int((seconds - int(seconds)) * 1000)
     return f"{hours:02d}:{minutes:02d}:{secs:02d},{millis:03d}"
 
+
 def save_srt(sentences, output_path):
+    """
+    Save sentences with timestamps to SRT file.
+    Merges very short sentences (<0.5s) with previous one for smoother reading.
+    """
     lines = []
     buffer_sentence = None
-    for s in sentences:
+
+    for idx, s in enumerate(sentences, start=1):
+        # Merge tiny sentences with previous
         duration = s['end'] - s['start']
         if duration < 0.5 and buffer_sentence is not None:
             buffer_sentence['text'] += " " + s['text'].strip()
             buffer_sentence['end'] = s['end']
             continue
         else:
-            if buffer_sentence:
-                lines.append(f"{len(lines)//4+1}\n{seconds_to_srt_time(buffer_sentence['start'])} --> {seconds_to_srt_time(buffer_sentence['end'])}\n{buffer_sentence['text'].strip()}\n")
+            if buffer_sentence is not None:
+                start_time = seconds_to_srt_time(buffer_sentence['start'])
+                end_time = seconds_to_srt_time(buffer_sentence['end'])
+                lines.append(f"{len(lines)//4 + 1}\n{start_time} --> {end_time}\n{buffer_sentence['text'].strip()}\n")
             buffer_sentence = s
-    if buffer_sentence:
-        lines.append(f"{len(lines)//4+1}\n{seconds_to_srt_time(buffer_sentence['start'])} --> {seconds_to_srt_time(buffer_sentence['end'])}\n{buffer_sentence['text'].strip()}\n")
+
+    # Write the last buffered sentence
+    if buffer_sentence is not None:
+        start_time = seconds_to_srt_time(buffer_sentence['start'])
+        end_time = seconds_to_srt_time(buffer_sentence['end'])
+        lines.append(f"{len(lines)//4 + 1}\n{start_time} --> {end_time}\n{buffer_sentence['text'].strip()}\n")
+
     with open(output_path, "w", encoding="utf-8") as f:
         f.writelines(lines)
+    print(f"SRT saved to {output_path}")
     return output_path
 
-srt_filename = local_audio_file.replace(".wav", ".srt")
+# Save SRT locally
+srt_filename = audio_path.replace(".wav", ".srt")
 save_srt(all_sentences, srt_filename)
-s3_client.put_object(Body=open(srt_filename, "rb"), Bucket=output_bucket_name, Key=output_file_prefix + srt_filename)
 
-print(f"TXT & SRT uploaded to S3 at {output_file_prefix}{txt_filename} & {output_file_prefix}{srt_filename}")
+# Upload SRT to S3
+s3_client.put_object(
+    Body=open(srt_filename, "rb"),
+    Bucket=output_bucket_name,
+    Key=output_file_prefix + srt_filename
+)
+print(f"SRT uploaded to S3 at {output_file_prefix + srt_filename}")
